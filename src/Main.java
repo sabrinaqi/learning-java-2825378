afc725a9b7de15629c39465885d39c5098ee80ac
sabrinaqi <sabrina.qi@duke.edu>
2021-01-16 15:40:40 -0600

    wrote a MC question

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "What is the capital of New York?";
+        String choiceOne = "new york city";
+        String choiceTwo = "albany";
+        String choiceThree = "buffalo";
 
         String correctAnswer = choiceTwo;
 
-        // Write a print statement asking the question
-        // Write a print statement giving the answer choices
-
-        // Have the user input an answer
-        // Retrieve the user's input
-
-        // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
-
-        // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
-
+        System.out.println(question);
+        System.out.println(choiceOne + ", " + choiceTwo + ", or " + choiceThree);
+        Scanner userInput = new Scanner(System.in);
+        String userChoice = userInput.next().toLowerCase();
+        if (userChoice.equals(correctAnswer)) {
+            System.out.println("That is correct! Congratulations!");
+        } else {
+            System.out.println("Sorry, that is incorrect. The capital of New York is " + correctAnswer);
+        }
     }
 
 }
